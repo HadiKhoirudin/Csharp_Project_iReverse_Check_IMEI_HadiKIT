d40c3abbe67fddb438a41cd8eb4590ea328be75c
“HadiKhoirudin” <347extreme@gmail.com>
2023-05-21 16:09:14 +0700

    Initial Commit

---
+++ Sources/Main.Designer.cs
@@ -0,0 +1,122 @@
+﻿
+namespace iReverse_Check_IMEI_HadiK_IT
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.richTextBox_Log = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_imei = new System.Windows.Forms.TextBox();
+            this.button_check = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.richTextBox_Log);
+            this.groupBox1.Location = new System.Drawing.Point(13, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(384, 134);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Logs :";
+            // 
+            // richTextBox_Log
+            // 
+            this.richTextBox_Log.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.richTextBox_Log.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox_Log.Enabled = false;
+            this.richTextBox_Log.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox_Log.Location = new System.Drawing.Point(2, 19);
+            this.richTextBox_Log.Name = "richTextBox_Log";
+            this.richTextBox_Log.Size = new System.Drawing.Size(381, 109);
+            this.richTextBox_Log.TabIndex = 1;
+            this.richTextBox_Log.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(10, 164);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(49, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "IMEI : ";
+            // 
+            // textBox_imei
+            // 
+            this.textBox_imei.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox_imei.Location = new System.Drawing.Point(56, 160);
+            this.textBox_imei.Name = "textBox_imei";
+            this.textBox_imei.Size = new System.Drawing.Size(340, 26);
+            this.textBox_imei.TabIndex = 3;
+            // 
+            // button_check
+            // 
+            this.button_check.Location = new System.Drawing.Point(159, 212);
+            this.button_check.Name = "button_check";
+            this.button_check.Size = new System.Drawing.Size(92, 33);
+            this.button_check.TabIndex = 4;
+            this.button_check.Text = "CEK IMEI";
+            this.button_check.UseVisualStyleBackColor = true;
+            this.button_check.Click += new System.EventHandler(this.button_check_Click);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(409, 257);
+            this.Controls.Add(this.button_check);
+            this.Controls.Add(this.textBox_imei);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "iReverse Kemenperin Check IMEI - @HadiKIT";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RichTextBox richTextBox_Log;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_imei;
+        private System.Windows.Forms.Button button_check;
+    }
+}
+
---
+++ Sources/Main.cs
@@ -0,0 +1,51 @@
+﻿// Coded by Hadi Khoirudin, S.Kom.
+// 
+// Disclaimer :
+// Cek IMEI ini menggunakan API dari repositori https://github.com/hunternblz/bulk-cek-imei-kemenperin.git
+// Segala kesalahan maupun kegiatan yang berkaitan dengan pihak Kemenperin itu diluar tanggung jawab saya.
+// Saya hanya sebatas menggunakan layanan yang disediakan publik oleh pihak penyedia layanan API tersebut
+// Maka silahkan hubungi penyedia layanan API jika hal ini menyalahi aturan.
+
+using System;
+using System.Collections.Specialized;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+
+namespace iReverse_Check_IMEI_HadiK_IT
+{
+    public partial class Main : Form
+    {
+        WebClient wc = new WebClient();
+        NameValueCollection dataToSend = new NameValueCollection();
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button_check_Click(object sender, EventArgs e)
+        {
+            if (textBox_imei.Text.Length == 15)
+            {
+                richTextBox_Log.Clear();
+
+                dataToSend["imei"] = textBox_imei.Text;
+                string GetData = Encoding.UTF8.GetString(wc.UploadValues(@"https://api.nabil.my.id/cekImeiKemenperin", dataToSend));
+
+                richTextBox_Log.Text = GetData.Replace("{", "").Replace("}", "").Replace("\"", "").Replace("status", "").Replace("sukses", "").Replace("failed", "").Replace("message", "").Replace(":", "").Replace(",", "").Replace("     ", "").Replace("\n", "");
+            }
+            else
+            {
+                const string message = "Silahkan Masukan 15 Digit Angka IMEI";
+                const string caption = "Terjadi Kesalahan!";
+                var result = MessageBox.Show(
+                    message, caption,
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error
+                    );
+            }
+            
+        }
+    }
+}
